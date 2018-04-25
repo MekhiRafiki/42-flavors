cb5d86dd42fe6dc4bd58cece0dfd333dd8fa3632
Mekhi Jones <mekhi.rafiki@gmail.com>
2018-04-25 05:08:29 -0700

    task 1 - 4.5
    Push the first initial task of functionality up . Still working on decrementing and then highlight.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 /* Sets a random integer quantity in range [1, 20] for each flavor. */
 function setQuantities() {
-  // TODO
+  // Get all the elements that need to be modifed
+  var elements = document.getElementsByClassName("flavor");
+
+  Array.from(elements).forEach(function(element){
+    // Prepend the quantity as the first child of meta
+      var metaEl = element.querySelector(".meta");
+      var spanAdd = document.createElement("span");
+      spanAdd.classList.add("quantity");
+      spanAdd.innerHTML = Math.floor(Math.random() * 20);
+      metaEl.insertBefore(spanAdd, metaEl.firstChild);
+
+  });
 }
 
 /* Extracts and returns an array of flavor objects based on data in the DOM. Each
@@ -13,32 +24,70 @@ function setQuantities() {
  * quantity: how many cups of the flavor are available
  */
 function extractFlavors() {
-  // TODO
+  var flavors = [];
+
+    var elements = document.getElementsByClassName("flavor");
+    Array.from(elements).forEach(function(element){
+        var flavor = {
+          element: element,
+          name: element.querySelector('.description').querySelector('h2').innerText,
+          description:  element.querySelector('.description').querySelector('p').innerText,
+          price: parseFloat(element.querySelector('.meta').querySelector('.price').innerText.replace(/\$/g, '')),
+          quantity: element.querySelector('.meta').querySelector('span').innerText,
+        };
+        flavors.push(flavor);
+    });
+    return flavors;
 }
 
 /* Calculates and returns the average price of the given set of flavors. The
  * average should be rounded to two decimal places. */
 function calculateAveragePrice(flavors) {
-  // TODO
+  var total = 0;
+  var count = 0;
+  Array.from(flavors).forEach(function(element){
+      total = total + element['price'];
+      count++;
+  });
+  var averageI = total / count;
+  return averageI.toFixed(2);
 }
 
 /* Finds flavors that have prices below the given threshold. Returns an array
  * of strings, each of the form "[flavor] costs $[price]". There should be
  * one string for each cheap flavor. */
 function findCheapFlavors(flavors, threshold) {
-  // TODO
+  flavors = Array.from(flavors).filter(function(flavor){
+    return flavor['price'] <= threshold;
+  });
+  return Array.from(flavors).map(flavor => flavor['name'] + " cost $" + flavor['price']);
 }
 
 /* Populates the select dropdown with options. There should be one option tag
  * for each of the given flavors. */
 function populateOptions(flavors) {
-  // TODO
+  var dropdown = document.querySelector('select');
+  Array.from(flavors).forEach(function(flavor){
+      var op = document.createElement('option');
+      op.innerHTML = flavor['name'];
+      op.value = flavor['name'];
+      dropdown.appendChild(op);
+  });
 }
 
 /* Processes orders for the given set of flavors. When a valid order is made,
  * decrements the quantity of the associated flavor. */
 function processOrders(flavors) {
-  // TODO
+  var submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', function(event){
+    event.preventDefault();
+    var dropdown = document.querySelector('select');
+    var flavor = dropdown.value;
+    var amount = document.querySelector('input[type="text"]').value
+
+    // Handle updating
+    var chosenOp = document.querySelector('select').querySelector('option[value="BERRIED TREASURE"]');
+  });
 }
 
 /* Highlights flavors when clicked to make a simple favoriting system. */
